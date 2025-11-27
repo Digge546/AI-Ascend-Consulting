997b85957a66393cd40b520a2db28b61e4052553
Digge546 <digge546@gmail.com>
2025-11-26 23:29:25 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let currentIndex = 0;
-const items = document.querySelectorAll(".carousel .service-item");
-
-function moveSlide(step) {
-    currentIndex += step;
-    if (currentIndex >= items.length) currentIndex = 0;
-    if (currentIndex < 0) currentIndex = items.length - 1;
-    document.querySelector(".carousel").style.transform = `translateX(-${currentIndex * 100}%)`;
-}
